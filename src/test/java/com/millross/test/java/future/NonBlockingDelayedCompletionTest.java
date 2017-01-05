7472fa583377cf50b2730751d7f614974dc57fae
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-01-05 18:58:51 +0000

    Issue #1 Demo for completable future using delayed non-blocking computation
    This is to test use of a completable future being completed within a vert.x environment following some time-consuming but non-blocking operation.
    
    The aim is to ensure that subsequent processing occurs on the correct thread and correct context.

---
+++ src/test/java/com/millross/test/java/future/NonBlockingDelayedCompletionTest.java
@@ -0,0 +1,41 @@
+package com.millross.test.java.future;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.RunTestOnContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ *
+ */
+@RunWith(VertxUnitRunner.class)
+public class NonBlockingDelayedCompletionTest {
+
+    @Rule
+    public final RunTestOnContext rule = new RunTestOnContext();
+
+    @Test
+    public void testNonBlockingDelayedCompletion(TestContext context) {
+
+        final Async async = context.async();
+        final Vertx vertx = rule.vertx();
+        final CompletableFuture<Integer> toComplete = new CompletableFuture<>();
+        // delay future completion by 500 ms
+        final String threadName = Thread.currentThread().getName();
+        vertx.setTimer(500, i -> toComplete.complete(100));
+        toComplete.thenRun(() -> {
+            assertThat(Thread.currentThread().getName(), is(threadName));
+            async.complete();
+        });
+    }
+
+}
