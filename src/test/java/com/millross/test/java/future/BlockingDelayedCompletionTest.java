2a7f7c8417a493e2be9f664e83a31da414d271ee
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-01-07 14:35:16 +0000

    vertx-completablefuture-demo-5 Provide an "incorrect" approach test for completable future using ...

--- src/test/java/com/millross/test/java/future/BlockingDelayedCompletionTest.java
+++ src/test/java/com/millross/test/java/future/BlockingDelayedCompletionTest.java
@@ -6,6 +6,7 @@ import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.RunTestOnContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +14,7 @@ import org.junit.runner.RunWith;
 import java.util.concurrent.CompletableFuture;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -24,6 +26,11 @@ public class BlockingDelayedCompletionTest {
 
     @Rule
     public final RunTestOnContext rule = new RunTestOnContext();
+
+    @Before
+    public void setupExceptionHandler(TestContext context) {
+        rule.vertx().exceptionHandler(context.exceptionHandler());
+    }
 
     @Test
     public void testBlockingDelayedCompletionCorrectlyWrapped(TestContext context) {
@@ -46,6 +53,42 @@ public class BlockingDelayedCompletionTest {
 
         toComplete.thenRun(() -> {
             assertThat(Thread.currentThread().getName(), is(threadName));
+            async.complete();
+        });
+    }
+
+    /**
+     * Test to show that if we wrap the blocking code incorrectly (by completing the future
+     * within the blocking code rather than in the async result handler for it) then our
+     * future completion handler will not occur on the desired thread, breaking any threading model
+     * in place.This is significant for, for example, vert.x, where each verticle instance
+     * is bound to a specific thread and future completion must happen on that thread if we want to ensure that
+     * all relevant objects will be treated safely. Very significant for http handling where the relevant classes
+     * are not expected to be thread-safe because they are expected to be bound to a single verticle's event loop.s
+     * @param context
+     */
+    @Test
+    public void testBlockingDelayedCompletionIncorrectlyWrapped(TestContext context) {
+
+        final Async async = context.async();
+        final Vertx vertx = rule.vertx();
+        final CompletableFuture<Integer> toComplete = new CompletableFuture<>();
+        // delay future completion by 500 ms
+        final String threadName = Thread.currentThread().getName();
+
+        vertx.executeBlocking((Future<Void> future) -> {
+            try {
+                Thread.sleep(1000);
+                toComplete.complete(1000);
+                future.complete(null);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                context.fail("Sleep failed");
+            }
+        }, false, result -> {});
+
+        toComplete.thenRun(() -> {
+            assertThat(Thread.currentThread().getName(), is(not(threadName)));
             async.complete();
         });
     }
